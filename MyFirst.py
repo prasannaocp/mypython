4cb2b3a99f170597dc5b71255c631144248fe5c0
prasannaocp <37493982+prasannaocp@users.noreply.github.com>
2018-03-25 17:20:48 -0400

    Add files via upload

---
+++ MyFirst.py
@@ -0,0 +1,26 @@
+print ("This is my first python Program")
+print ("this is Prasanna's first python program!")
+print ('This is Prasanna"S first program and learning string and Variables')
+print ("Python is \"fun\" to learn and remember it's \"easy to forget\"")
+print ('Python is \"fun\" to learn and remember it\'s \"easy to forget\"')
+name = 'Prasanna Sridharan'
+print(name)
+print (name.upper())
+print (name.lower())
+length_name = len(name)
+print(length_name)
+print ("I" + "Learn" + "Concatenation")
+print ("I" + " Learn " + "Concatenation")
+action = 'reading'
+subject = 'python'
+sentence = name + action + subject
+print(sentence)
+sentence1 = name + ' ' + action + " " + subject + "."
+print(sentence1)
+mean = 'Be Happy ' + 'always '
+print (mean * 5)
+version = 2.0
+print (name + " " + str(version))
+print ('{} {} {},'.format('I', 'Love', 'Python'))
+print ("I hate {2}".format('I', 'Love', 'Python'))
+print("length of " + (name)+'is '+ str(length_name))
