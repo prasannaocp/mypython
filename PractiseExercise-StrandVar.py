ddf7063ee561f7adf340dcefbc792093ffbc7db7
prasanna <prasannaocp@gmail.com>
2018-03-26 21:31:34 -0400

    Learning Strings from Udemy1
    Signed-off-by: prasanna <prasannaocp@gmail.com>

---
+++ PractiseExercise-StrandVar.py
@@ -0,0 +1,18 @@
+animal='cat'
+vegetable='broccoli'
+mineral='gold'
+print('Here is an animal, a vegetable, and a mineral')
+print(animal)
+print(vegetable)
+print(mineral)
+
+
+a= raw_input('Please type something and Press enter:')
+print ('You entered:')
+print (a)
+
+user_input = raw_input('Please type something and press enter: ')
+print('You entered:')
+print(user_input)
+
+
