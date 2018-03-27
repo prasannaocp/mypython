16337f0cc53d3ad88bfd8fae6a40033444f61d36
prasanna <prasannaocp@gmail.com>
2018-03-26 21:35:08 -0400

    Learning Strings from Udemy1
    Signed-off-by: prasanna <prasannaocp@gmail.com>

--- PractiseExercise-StrandVar.py
+++ PractiseExercise-StrandVar.py
@@ -6,13 +6,8 @@ print(animal)
 print(vegetable)
 print(mineral)
 
-
 a= raw_input('Please type something and Press enter:')
 print ('You entered:')
 print (a)
-
-user_input = raw_input('Please type something and press enter: ')
-print('You entered:')
-print(user_input)
 
 
