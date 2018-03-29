550bcf765e71886422e5d2d0449a30fbb386c2f9
prasanna <prasannaocp@gmail.com>
2018-03-28 20:49:47 -0400

    Learning Strings from Udemy1
    Signed-off-by: prasanna <prasannaocp@gmail.com>

---
+++ Numeric.py
@@ -0,0 +1,23 @@
+# Boolean Operators
+
+a = 1
+b = 2
+print a == b # equal to
+print a > b # greater
+print a < b # lesser
+print a != b  # Not Equal to
+print a <= b # Lesser than equal
+print a >= b # greater than equal
+
+#and
+#or
+#not
+
+print 37 > 29 and 37 > 31
+print 37 > 29 or 37 > 31
+
+print 40 < 20 and 40<20
+print "to be contd.."
+
+
+
