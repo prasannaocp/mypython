2790d53b06f76030870f1e61b70fd08c92e2a785
prasanna <prasannaocp@gmail.com>
2018-04-08 19:45:49 -0400

    Basic Python Programming
    Signed-off-by: prasanna <prasannaocp@gmail.com>

---
+++ forloop.py
@@ -0,0 +1,10 @@
+#loops
+#for - every items on list - code block used
+
+sports=['WeE','CricKet','Tennis','Hockey','Football']
+for sports_name in sports:
+     print (sports_name.upper())
+
+#learn more
+
+     
---
+++ whileloop.py
@@ -0,0 +1,9 @@
+#while loop
+#infinite loop - ctl+C
+
+sports=['WeE','CricKet','Tennis','Hockey','Football']
+index=0
+
+while index<len(sports):
+    print (sports[index])
+    index +=1
